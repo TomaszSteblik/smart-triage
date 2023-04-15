f44b0794b15ea89f2ba90f46ec2de3001024f547
rtrydev <r.kulka44@gmail.com>
2023-04-15 22:05:20 +0200

    imp

--- github-service/app.py
+++ github-service/app.py
@@ -1,18 +1,56 @@
 from fastapi import FastAPI, Request
 from urllib.parse import unquote
 import json
+import requests
+import jwt
+import os
+import time
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+@app.get("/issues")
+def read_issues():
+  response = requests.get("https://api.github.com/repos/TomaszSteblik/smart-triage/issues")
+  data = response.json()
 
-@app.post("/")
+  return data
+
+@app.post("/issues")
+def update_issue(resquest: Request):
+  pem = "smarttriage.2023-04-15.private-key.pem"
+  with open(pem, 'rb') as pem_file:
+    signing_key = jwt.jwk_from_pem(pem_file.read())
+
+  app_id = os.environ.get('APP_ID')
+
+  payload = {
+    'iat': int(time.time()),
+    'exp': int(time.time()) + 600,
+    'iss': app_id
+  }
+
+  jwt_instance = jwt.JWT()
+  encoded_jwt = jwt_instance.encode(payload, signing_key, alg='RS256')
+
+  headers = {
+    "Authorization": f"Bearer {encoded_jwt}"
+  }
+
+  # url = 'https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/comments'
+
+  # payload = {'body': 'This is a comment.'}
+  # response = requests.post(url.format(owner='TomaszSteblik', repo='smart-triage', issue_number='10'), headers=headers, json=payload)
+
+  # print(response.json())
+
+  return {
+    'status': 200
+  }
+
+@app.post("/webhook")
 async def process_webhook(request: Request):
   body = await request.body()
-  decoded = unquote(body[8:])
-  body_json = json.loads(decoded)
+  body_json = json.loads(body)
 
   issue = body_json.get('issue', {})
   title = issue.get('title')
@@ -21,7 +59,7 @@ async def process_webhook(request: Request):
 
   print(title, description)
   return {
-    'status_code': 200
+    'status': 200
   }
 
 
